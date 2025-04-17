2656d9ec6b037b47c6ca4539d7e5706839382edb
Ebenezer Gasonoo <gas24001@byui.edu>
2025-04-17 06:15:00 +0000

    page created

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -1,9 +1,12 @@
-document.getElementById("year").textContent = new Date().getFullYear();
-document.getElementById("lastModified").textContent = document.lastModified;
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("lastModified").textContent = document.lastModified;
+  const yearSpan = document.getElementById("year");
+  if (yearSpan) yearSpan.textContent = new Date().getFullYear();
 
-const menuToggle = document.getElementById("menu-toggle");
-const navMenu = document.getElementById("nav-menu");
+  const menuToggle = document.getElementById("menu-toggle");
+  const navMenu = document.getElementById("nav-menu");
 
-menuToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("open");
+  menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("open");
+  });
 });
